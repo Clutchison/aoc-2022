c6a072b20493285ce52a4137bccb947edffe965b
Sean Hutchison <xskhutchx@gmail.com>
2022-12-07 14:13:08 -0700

    d4p2

--- days/dayFour/dayFour.ts
+++ days/dayFour/dayFour.ts
@@ -4,20 +4,19 @@ import { Pair } from "./model/pair";
 export class DayFour extends Day {
   run(): void {
     let inputs = super.getInput() // Get Input
-      .split('\r\n');
+      .split('\r\n')
+      .map(i => new Pair(i));
 
     this.partOne(inputs);
     this.partTwo(inputs)
   }
 
-  private partOne(inputs: string[]) {
-    this.logPartOne(inputs.map(i => new Pair(i))
-      .filter(pair => pair.fullyContained())
-      .length);
+  private partOne(inputs: Pair[]) {
+    this.logPartOne(inputs.filter(pair => pair.fullyContained()).length);
   }
 
-  private partTwo(inputs: number[]) {
-    this.logPartTwo('Unsolved');
+  private partTwo(inputs: Pair[]) {
+    this.logPartTwo(inputs.map(p => p.overlap()).filter(r => r.length > 0).length);
   }
 
   toString(): string {
